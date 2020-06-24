b64165c11fff95082c5fc9a41ef11db75d9cc40b
mp3 <im.in.a.coma.xxx@gmail.com>
2020-06-24 17:54:50 +0900

    copy for adbanced examples

---
+++ advanced/track_changes/src/index.tsx
@@ -0,0 +1,13 @@
+import { h, render } from 'preact'
+
+const Main = () => {
+  return (
+    <div>examples</div>
+  )
+}
+
+const root = document.getElementById('root')
+
+if (root) [
+  render(<Main />, root)
+]
