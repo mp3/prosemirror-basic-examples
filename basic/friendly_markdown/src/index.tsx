673aacd5d8ca28dc86cb05c1d0b46bdea53c8b9f
mp3 <im.in.a.coma.xxx@gmail.com>
2020-06-25 14:57:54 +0900

    create a simple interface around a textarea

--- basic/friendly_markdown/src/index.tsx
+++ basic/friendly_markdown/src/index.tsx
@@ -1,8 +1,36 @@
 import { h, render } from 'preact'
+import { useRef, useEffect } from 'preact/hooks'
+
+class MarkdownView {
+  public textarea: HTMLTextAreaElement
+
+  constructor(target: Node, content: string) {
+    this.textarea = target.appendChild(document.createElement('textarea'))
+    this.textarea.value = content
+  }
+
+  get content() {
+    return this.textarea.value
+  }
+
+  focus() {
+    this.textarea.focus()
+  }
+
+  destroy() {
+    this.textarea.remove()
+  }
+}
 
 const Main = () => {
+  const editorRef = useRef<HTMLDivElement>(null)
+
+  useEffect(() => {
+
+  }, [])
+
   return (
-    <div>examples</div>
+    <div ref={editorRef} />
   )
 }
 
