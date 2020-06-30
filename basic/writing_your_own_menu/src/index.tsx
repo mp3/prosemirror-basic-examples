92094837ec088f29ad4aea1a19a568c35bf2bd52
mp3 <im.in.a.coma.xxx@gmail.com>
2020-06-30 13:18:07 +0900

    create MenuView class

--- basic/writing_your_own_menu/src/index.tsx
+++ basic/writing_your_own_menu/src/index.tsx
@@ -1,4 +1,41 @@
 import { h, render } from 'preact'
+import { EditorView } from 'prosemirror-view'
+
+class MenuView {
+  public editorView: EditorView
+  public dom: HTMLDivElement
+
+  constructor(items, editorView: EditorView) {
+    this.items = items
+    this.editorView = editorView
+
+    this.dom = document.createElement('div')
+    this.dom.className = 'menubar'
+    items.forEach(({ dom }) => this.dom.appendChild(dom))
+    this.update()
+
+    this.dom.addEventListener('mousedown', (event) => {
+      event.preventDefault()
+      editorView.focus()
+      items.forEach(({ command, dom }) => {
+        if (dom.contains(event.target)) {
+          command(editorView.state, editorView.dispatch, editorView)
+        }
+      })
+    })
+  }
+
+  update() {
+    this.items.forEach(({ command, dom }) => {
+      const active = command(this.editorView.state, null, this.editorView)
+      dom.style.display = active ? '' : 'none'
+    })
+  }
+
+  destroy() {
+    this.dom.remove()
+  }
+}
 
 const Main = () => {
   return (
