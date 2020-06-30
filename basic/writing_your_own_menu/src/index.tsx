f37eafb4c8f76ebd63fe933f468ef5fa5c193ad7
mp3 <im.in.a.coma.xxx@gmail.com>
2020-06-30 13:48:53 +0900

    implemented own menu

--- basic/writing_your_own_menu/src/index.tsx
+++ basic/writing_your_own_menu/src/index.tsx
@@ -1,5 +1,11 @@
 import { h, render } from 'preact'
+import { useEffect, useRef } from 'preact/hooks'
 import { EditorView } from 'prosemirror-view'
+import { Plugin, EditorState } from 'prosemirror-state'
+import { setBlockType, toggleMark, wrapIn, baseKeymap } from 'prosemirror-commands'
+import { schema } from 'prosemirror-schema-basic'
+import { DOMParser } from 'prosemirror-model'
+import { keymap } from 'prosemirror-keymap'
 
 class MenuView {
   public editorView: EditorView
@@ -38,9 +44,76 @@ class MenuView {
   }
 }
 
+const menuPlugin = (items) => {
+  return new Plugin({
+    view(editorView: EditorView) {
+      const menuView = new MenuView(items, editorView)
+      editorView.dom.parentNode?.insertBefore(menuView.dom, editorView.dom)
+      return menuView
+    }
+  })
+}
+
+const icon = (text: string, name: string) => {
+  const span = document.createElement('span')
+  span.className = `menuicon ${name}`
+  span.title = name
+  span.textContent = text
+  return span
+}
+
+const heading = (level: number) => {
+  return {
+    command: setBlockType(schema.nodes.heading, { level }),
+    dom: icon(`H${level}`, 'heading')
+  }
+}
+
+const menu = menuPlugin([
+  {
+    command: toggleMark(schema.marks.strong),
+    dom: icon('B', 'strong')
+  },
+  {
+    command: toggleMark(schema.marks.em),
+    dom: icon('i', 'em')
+  },
+  {
+    command: setBlockType(schema.nodes.paragraph),
+    dom: icon('p', 'paragraph')
+  },
+  heading(1),
+  heading(2),
+  heading(3),
+  {
+    command: wrapIn(schema.nodes.blockquote),
+    dom: icon('>', 'blockquote')
+  }
+])
+
 const Main = () => {
+  const editorRef = useRef<HTMLDivElement>(null)
+  const contentRef = useRef<HTMLDivElement>(null)
+
+  useEffect(() => {
+    const view = new EditorView(editorRef.current, {
+      state: EditorState.create({
+        doc: DOMParser.fromSchema(schema).parse(contentRef.current),
+        plugins: [keymap(baseKeymap), menu]
+      })
+    })
+
+    console.log(view)
+  })
+
   return (
-    <div>examples</div>
+    <div>
+      <div ref={editorRef} />
+      <div ref={contentRef} style="display: none">
+        <h3>Simple editor</h3>
+        <p>With a very crude menu bar.</p>
+      </div>
+    </div>
   )
 }
 
